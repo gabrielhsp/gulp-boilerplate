d4ba4b52fa8e9489f75daa3f5c1e04c7da485972
Gabriel Henrique <gabhenrique0@gmail.com>
2016-07-17 00:05:38 -0300

    Substitui o plugin gulp-connect-multi pelo plugin gulp-webserver

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,38 @@
-var gulp 	 = require('gulp');
-var connect  = require('gulp-connect-multi')();
-var cleanCSS = require('gulp-clean-css');
-var sass 	 = require('gulp-ruby-sass');
+var gulp 	  = require('gulp');
+var cleanCSS  = require('gulp-clean-css');
+var sass 	  = require('gulp-ruby-sass');
+var webserver = require('gulp-webserver');
 
 // Default task
-gulp.task('default', ['sass', 'clean-css', 'server', 'watch']);
+gulp.task('default', ['sass', 'clean-css', 'webserver', 'watch']);
 
 // Sass Task - Use to create sass task
 gulp.task('sass', function() {
     sass('assets/scss/**/*.scss')
         .on('error', sass.logError)
-        .pipe(gulp.dest('assets/css/'))
-        .pipe(connect.reload());
+        .pipe(gulp.dest('assets/css/'));
 });
 
 // Clean CSS Task
 gulp.task('clean-css', function() {
 	return gulp.src('assets/css/*.css')
 	.pipe(cleanCSS({compatibility: 'ie8'}))
-	.pipe(gulp.dest('assets/css/'))
-	.pipe(connect.reload());
+	.pipe(gulp.dest('assets/css/'));
 });
 
-// Connect Task - Use to create a server with livereload
-gulp.task('server', connect.server({
-	root: ['build'],
-	port: 9000,
-	livereload: true,
-	directoryListing: true,
-	open: {
-		browser: 'chrome'
-	}
-}));
+// Webserver task - Use to start a local webserver
+gulp.task('webserver', function() {
+	gulp.src('./')
+	.pipe(webserver({
+		fallback: 'index.html',
+		livereload: true,
+		open: true,
+		directoryListing: {
+			enable: true,
+			path: 'app'
+		}
+	}));
+});
 
 // Watch task - Use to watch change in your files and execute other tasks
 gulp.task('watch', function() {
